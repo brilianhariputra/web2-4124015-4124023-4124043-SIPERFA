e90a1635870645bd699789ba1e7255b53db1296e
brilanhariputra <brilianhariputra99@gmail.com>
2026-03-05 13:27:30 +0700

    perbaikan route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/brilianhariputra', function () {
+    return 'Halo, saya Brilian Hariputra. Ini rute kolaborasi saya!';
+});
