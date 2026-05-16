9c621d517ca68a0cc23f03ab2279ee6b9aa94153
Brilian Hariputra <brilianhariputra99@gmail.com>
2026-05-16 12:49:23 +0700

    fix: menyelesaikan konflik rute, migrasi sqlite, dan melengkapi fitur katalog serta produk

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SIPERFA</title>
+    <!-- Hubungkan file CSS Anda di sini, misalnya jika pakai Vite: -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <h1>Selamat Datang di Website Saya</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -9,14 +9,20 @@ use App\Http\Controllers\ProdukController;
 /*
 
 |--------------------------------------------------------------------------
-| Halaman Utama & Sambutan Anggota Tim
+| Halaman Utama Project
 |--------------------------------------------------------------------------
 */
-
+// Mengarahkan halaman utama '/' langsung ke view welcome yang sudah ber-CSS
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('SIPERFA.index');
 
+/*
+
+|--------------------------------------------------------------------------
+| Sambutan Anggota Tim
+|--------------------------------------------------------------------------
+*/
 Route::get('/brilianhariputra', function () {
     return 'Halo, saya Brilian Hariputra. Ini rute kolaborasi saya!';
 });
@@ -45,13 +51,9 @@ Route::get('/zulkhi', function () {
 | Static Routes (Informasi)
 |--------------------------------------------------------------------------
 */
-
-Route::get('/home', function () {
-    return "<h1>Selamat Datang di Website Saya</h1>";
-})->name('SIPERFA.index');
-
+// Menyatukan rute tentang agar seragam menggunakan file view 'tentang'
 Route::get('/about', function () {
-    return "<h1>Halaman Tentang Saya</h1>";
+    return view('tentang');
 })->name('about.index');
 
 Route::get('/tentang', function () {
@@ -59,7 +61,7 @@ Route::get('/tentang', function () {
 });
 
 Route::get('/kontak', function () {
-    return "<h1>Hubungi Saya di email:brilianhariputra99@gmail.com</h1>";
+    return "<h1>Hubungi Saya di email: brilianhariputra99@gmail.com</h1>";
 })->name('kontak.index');
 
 /*
@@ -69,20 +71,20 @@ Route::get('/kontak', function () {
 |--------------------------------------------------------------------------
 */
 
-// Profil
+// Fitur Profil
 Route::get('/profil', [ProfilController::class, 'index'])->name('profil.index');
 Route::get('/profil/{nim}', [ProfilController::class, 'show'])->name('profil.show');
 
-// Katalog
+// Fitur Katalog
 Route::get('/katalog', [KatalogController::class, 'index'])->name('katalog.index');
 Route::get('/katalog/{id}', [KatalogController::class, 'show'])->name('katalog.show');
 
-// Ruangan
+// Fitur Ruangan
 Route::get('/ruangan', [RuanganController::class, 'index'])->name('ruangan.index');
 Route::get('/ruangan/{id}', [RuanganController::class, 'show'])->name('ruangan.show');
 Route::get('/booking/{id}', [RuanganController::class, 'booking'])->name('ruangan.booking');
 
-// Produk
+// Fitur Produk
 Route::get('/produk', [ProdukController::class, 'index'])->name('produk.index');
 Route::get('/produk/create', [ProdukController::class, 'create'])->name('produk.create');
 Route::post('/produk', [ProdukController::class, 'store'])->name('produk.store');
