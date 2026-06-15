f02f5094a2d143ff84be5d970e39d0f9f8306ff2
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-16 00:58:59 +0700

    fix: final index.php configuration

--- api/index.php
+++ api/index.php
@@ -1,6 +1,22 @@
 <?php
-// Arahkan keluar satu tingkat (..) untuk mencapai vendor dan bootstrap
+// Paksa Environment
+putenv('APP_ENV=production');
+putenv('APP_DEBUG=true');
+putenv('CACHE_DRIVER=array');
+putenv('SESSION_DRIVER=array');
+putenv('QUEUE_CONNECTION=sync');
+putenv('VIEW_COMPILED_PATH=/tmp');
+
 require __DIR__ . '/../vendor/autoload.php';
+
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$app->handle(\Illuminate\Http\Request::capture())->send();
+// GUNAKAN HTTP KERNEL (Bukan Console Kernel)
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+$kernel->terminate($request, $response);
