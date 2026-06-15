38ce23672c113ccb2522ea43f425eb493faa276d
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-16 01:07:43 +0700

    fix: remove manual view singleton

--- api/index.php
+++ api/index.php
@@ -1,16 +1,21 @@
 <?php
-// Paksa semua ke memori
+// Paksa Environment
+putenv('APP_ENV=production');
 putenv('CACHE_DRIVER=array');
 putenv('SESSION_DRIVER=array');
-putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('QUEUE_CONNECTION=sync');
 
 require __DIR__ . '/../vendor/autoload.php';
+
+// Gunakan App, tapi jangan manipulasi container 'view' secara manual
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Paksa Laravel menggunakan konfigurasi yang aman untuk serverless
-$app->singleton('view', function () { return new stdClass; }); 
-
+// Gunakan Http Kernel untuk memproses request
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle($request = Illuminate\Http\Request::capture());
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
 $response->send();
 $kernel->terminate($request, $response);
