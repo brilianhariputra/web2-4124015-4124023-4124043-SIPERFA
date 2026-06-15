13fec6c1c95d7f825b001726391a43942c16341d
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-16 00:27:02 +0700

    fix: force move to api folder

--- api/index.php
+++ api/index.php
@@ -1,4 +1,6 @@
 <?php
+// Arahkan keluar satu tingkat (..) untuk mencapai vendor dan bootstrap
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
+
 $app->handle(\Illuminate\Http\Request::capture())->send();
