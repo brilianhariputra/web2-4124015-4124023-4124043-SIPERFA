ff4f4bdb7281519bc9441418d3d3511b1bd17d6e
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-16 10:27:48 +0700

    debug: check paths on Vercel

--- api/index.php
+++ api/index.php
@@ -1,25 +1,10 @@
 <?php
 
-define('LARAVEL_START', microtime(true));
-
 $basePath = dirname(__DIR__);
 
-require_once $basePath . '/vendor/autoload.php';
-
-$app = \Illuminate\Foundation\Application::configure(basePath: $basePath)
-    ->withRouting(
-        web: $basePath . '/routes/web.php',
-        commands: $basePath . '/routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (\Illuminate\Foundation\Configuration\Middleware $middleware) {})
-    ->withExceptions(function (\Illuminate\Foundation\Configuration\Exceptions $exceptions) {})
-    ->create();
-
-$kernel = $app->make(\Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = \Illuminate\Http\Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+echo "<pre>";
+echo "basePath: " . $basePath . "\n";
+echo "autoload exists: " . (file_exists($basePath . '/vendor/autoload.php') ? 'YES' : 'NO') . "\n";
+echo "bootstrap exists: " . (file_exists($basePath . '/bootstrap/app.php') ? 'YES' : 'NO') . "\n";
+echo "views exists: " . (file_exists($basePath . '/resources/views') ? 'YES' : 'NO') . "\n";
+echo "</pre>";
