0f8f59408e52c442e9f2f8fd46b66bbd2811d067
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-16 10:15:32 +0700

    fix: add missing use statements for Laravel 12

--- api/index.php
+++ api/index.php
@@ -2,19 +2,22 @@
 
 define('LARAVEL_START', microtime(true));
 
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
 $basePath = dirname(__DIR__);
 
 require_once $basePath . '/vendor/autoload.php';
 
-// Override basePath SEBELUM Application dibuat
 $app = Application::configure(basePath: $basePath)
     ->withRouting(
         web: $basePath . '/routes/web.php',
         commands: $basePath . '/routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function ($middleware) {})
-    ->withExceptions(function ($exceptions) {})
+    ->withMiddleware(function (Middleware $middleware) {})
+    ->withExceptions(function (Exceptions $exceptions) {})
     ->create();
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
