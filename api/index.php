35aaf3ca2d5c121c3869a0a3ba6ff3a6b24fee5d
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-16 10:50:02 +0700

    fix: recreate index.php clean

--- api/index.php
+++ api/index.php
@@ -1,17 +1,8 @@
-<?php
-
+﻿<?php
 define('LARAVEL_START', microtime(true));
-
 $basePath = dirname(__DIR__);
-
 require $basePath . '/vendor/autoload.php';
-
 $app = require $basePath . '/bootstrap/app.php';
-
 $kernel = $app->make(\Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = \Illuminate\Http\Request::capture()
-)->send();
-
+$response = $kernel->handle($request = \Illuminate\Http\Request::capture())->send();
 $kernel->terminate($request, $response);
