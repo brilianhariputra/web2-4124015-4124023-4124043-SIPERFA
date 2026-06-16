914108044214e4d8a6a8e3e871f103104cfb447a
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-16 11:04:17 +0700

    fix: move entry point to root index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+define('LARAVEL_START', microtime(true));
+$basePath = __DIR__;
+require $basePath . '/vendor/autoload.php';
+$app = require $basePath . '/bootstrap/app.php';
+$kernel = $app->make(\Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle($request = \Illuminate\Http\Request::capture())->send();
+$kernel->terminate($request, $response);
