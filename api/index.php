1a5716a83ba82d68f45e363f34bdf3eed144a170
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-29 14:39:33 +0700

    debug: tampilkan error asli

--- api/index.php
+++ api/index.php
@@ -2,13 +2,29 @@
 
 define('LARAVEL_START', microtime(true));
 
+// Buat folder /tmp
+$dirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+    '/tmp/bootstrap/cache',
+];
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) mkdir($dir, 0755, true);
+}
+
 if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-$app->useStoragePath('/tmp/storage');
-
-$app->handleRequest(Illuminate\Http\Request::capture());
+try {
+    $app = require_once __DIR__ . '/../bootstrap/app.php';
+    $app->handleRequest(Illuminate\Http\Request::capture());
+} catch (\Throwable $e) {
+    http_response_code(500);
+    echo "<h1>Error:</h1>";
+    echo "<pre>" . $e->getMessage() . "\n\n" . $e->getTraceAsString() . "</pre>";
+}
