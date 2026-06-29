18497d114f4ef2adc30b202a058772463027e3a2
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-29 14:24:11 +0700

    fix: buat /tmp dirs di bootstrap

--- api/index.php
+++ api/index.php
@@ -9,19 +9,6 @@ if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php'
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// Gunakan /tmp untuk storage yang writable di Vercel
 $app->useStoragePath('/tmp/storage');
-
-// Buat folder yang dibutuhkan di /tmp
-$dirs = [
-    '/tmp/storage/framework/views',
-    '/tmp/storage/framework/cache/data',
-    '/tmp/storage/framework/sessions',
-    '/tmp/storage/logs',
-];
-foreach ($dirs as $dir) {
-    if (!is_dir($dir)) mkdir($dir, 0755, true);
-}
 
 $app->handleRequest(Illuminate\Http\Request::capture());
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,20 @@
-
 <?php
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+
+// Buat folder /tmp yang dibutuhkan
+$dirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+    '/tmp/bootstrap/cache',
+];
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) mkdir($dir, 0755, true);
+}
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
