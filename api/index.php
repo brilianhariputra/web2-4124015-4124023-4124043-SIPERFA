f05445ae5d40e939f1112f0b839fe17d147b1635
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-29 13:40:32 +0700

    fix: update api/index.php dengan path eksplisit

--- api/index.php
+++ api/index.php
@@ -1,4 +1,13 @@
 <?php
 
-// Meneruskan request Vercel ke index.php bawaan Laravel
-require __DIR__ . '/../public/index.php';
+define('LARAVEL_START', microtime(true));
+
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->handleRequest(Illuminate\Http\Request::capture());
