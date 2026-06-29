3c841193ff73acd789f4d1dec0d7fbf871ca35ec
brilanhariputra <brilianhariputra99@gmail.com>
2026-06-29 14:11:34 +0700

    debug: step by step

--- api/index.php
+++ api/index.php
@@ -3,14 +3,12 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-define('LARAVEL_START', microtime(true));
-
-if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+echo "Step 1: PHP works<br>";
 
 require __DIR__ . '/../vendor/autoload.php';
+echo "Step 2: Autoload OK<br>";
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
+echo "Step 3: Bootstrap OK<br>";
 
 $app->handleRequest(Illuminate\Http\Request::capture());
